86c06af499c3c0506e00d453d6cf879bf23c981a
kazusawatanabe <kazusa-r34camera@softbank.ne.jp>
2020-10-22 17:54:26 +0900

    add files

---
+++ elevator/move.c
@@ -0,0 +1,7 @@
+//
+//  move.c
+//  kit-elevator-simulation
+//
+//  Created by Kazusa Watanabe on 2020/10/22.
+//  Copyright (c) 2020 Kazusa Watanabe. All rights reserved.
+//
---
+++ elevator/move.h
@@ -0,0 +1,7 @@
+//
+//  move.h
+//  kit-elevator-simulation
+//
+//  Created by kazusa watanabe on 2020/10/22.
+//  Copyright (c) 2020 \Kazusa Watanabe. All rights reserved.
+//
