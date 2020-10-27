510ee5e61af8523da4d4455b504ad42c1008ca1a
Shumpei Kawaguchi <53923413+shumpei-kawaguchi@users.noreply.github.com>
2020-10-27 22:12:29 +0900

    fix ratio.c

--- calculations/ratio.c
+++ calculations/ratio.c
@@ -14,44 +14,43 @@
 
 #include "../common/define.h"
 
-int classroom_of_level[LEVEL] = {1, 3, 3, 3, 2, 2, 1};
-int ratio_of_level[LEVEL] = {};
+static int classroom_of_level[LEVEL] = {1, 3, 3, 3, 2, 2, 1};
+static int ratio_of_level[LEVEL] = {};
 
+// Return classroom of level.
 unsigned int classroom_of(int level) { return classroom_of_level[level]; }
 
+// Return ratio of level.
 unsigned int ratio_of(int level) { return ratio_of_level[level]; }
 
+// Set ratio value of level.
 unsigned int ratio_is(int level, int value) {
   ratio_of_level[level] = value;
   return ratio_of_level[level];
 }
 
+// Set ratio pattern.
 int ratio_pattern() {
-  //
-  srand((unsigned int)time(NULL));
   int classroom = CLASS;
-  //
+  int total = 0;
+  srand((unsigned int)time(NULL));
+
   for (int i = 0; i < LEVEL; i++) {
-    //
     int max = 0;
-    for (int next = i; next < LEVEL; next++) {
-      max += classroom_of(next);
-    }
-    //
     int include = 0;
     int col = classroom_of(i);
+    for (int next = i; next < LEVEL; next++) max += classroom_of(next);
+
     if (max - col - classroom < 0) include = -(max - col - classroom);
     ratio_is(i, classroom_ratio(i, include, classroom));
     classroom -= ratio_of(i);
   }
-  //
-  int total = 0;
-  for (int i = 0; i < LEVEL; i++) total += ratio_of(i);
 
-  //
+  for (int i = 0; i < LEVEL; i++) total += ratio_of(i);
   return CLASS - total;
 }
 
+// Return randam ratio.
 int classroom_ratio(int n, int include, int classroom) {
   int num = rand() % (classroom_of(n) + 1);
   if (num < include) num = include;
