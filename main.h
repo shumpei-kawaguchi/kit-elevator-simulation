bc98ccdb7362898eb91ebb0fdfe7691253723114
Shumpei Kawaguchi <53923413+shumpei-kawaguchi@users.noreply.github.com>
2020-11-04 12:50:36 +0900

    add main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+//
+//  main.h
+//  kit-elevator-simulation
+//
+//  Created by Shumpei Kawaguchi on 2020/11/04.
+//  Copyright (c) 2020 Shumpei Kawaguchi. All rights reserved.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int setup(void);
+int main(void);
+
+#endif
