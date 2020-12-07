c98d4289eda10f48f2d730cc8c1758dbf2fc55ae
Shumpei Kawaguchi <53923413+shumpei-kawaguchi@users.noreply.github.com>
2020-12-07 23:27:29 +0900

    fix

--- elevator/upPeakTraffic.h
+++ elevator/upPeakTraffic.h
@@ -12,7 +12,6 @@
 #include "../common/define.h"
 
 static inline MODEL up_peak_traffic(double average) {
-  // p->average = service_average();
   // １秒当たりの、到着率
   double p1 = (double)(CLASS * NUMBER_OF_PEOPLE) / (10 * 60);
   // 一人当たりの、サービス率
--- main.h
+++ main.h
@@ -50,18 +50,6 @@ static inline SETTINGS setup(void) {
   return st;
 }
 
-// static inline MODEL up_peak_traffic() {
-//   p->average = service_average();
-//   // １秒当たりの、到着率
-//   double p1 = (double)(CLASS * NUMBER_OF_PEOPLE) / (10 * 60);
-//   // 一人当たりの、サービス率
-//   double p2 = 1 / (p->average / BOX);
-
-//   MODEL model = {p1, p2, SERVER};
-
-//   return model;
-// }
-
 static inline double convergence() {
   variances variances = {0.0, 0.0, 0};
 
