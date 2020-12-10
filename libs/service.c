78c0666fbb6cf08095cfe50af866d7eba5cb5c8e
Shumpei Kawaguchi <53923413+shumpei-kawaguchi@users.noreply.github.com>
2020-12-10 21:23:59 +0900

    add service file

---
+++ libs/service.c
@@ -0,0 +1,7 @@
+//
+//  service.c
+//  kit-elevator-simulation
+//
+//  Created by Shumpei Kawaguchi on 2020/12/10.
+//  Copyright (c) 2020 Shumpei Kawaguchi. All rights reserved.
+//
---
+++ libs/service.h
@@ -0,0 +1,15 @@
+//
+//  service.h
+//  kit-elevator-simulation
+//
+//  Created by Shumpei Kawaguchi on 2020/12/10.
+//  Copyright (c) 2020 Shumpei Kawaguchi. All rights reserved.
+//
+
+#ifndef SERVICE_H
+#define SERVICE_H
+
+#include "calculations.h"
+#include "define.h"
+
+#endif
